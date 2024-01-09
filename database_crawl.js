cf7e8ebb97045d045c801243a0c1c1504ff64422
Emily Sunaryo <eadorasunaryo@berkeley.edu>
2024-01-09 04:51:53 -0800

    progress on populating custom object

--- database_crawl.js
+++ database_crawl.js
@@ -8,7 +8,6 @@ async function getSpotifyTrackIDs() {
     const tokenResponse = await getToken();
     const accessToken = tokenResponse.access_token;
 
-    //still have access to the wiki id in the songData 
     for (const song of songData) {
         const artistName = song.artist;
         const songTitle = song.song;
@@ -18,10 +17,6 @@ async function getSpotifyTrackIDs() {
             const trackId = await searchTrack(artistName, songTitle, accessToken);
             console.log("Spotify ID:", trackId);
             spotifyIDs[songID] = trackId;
-            // spotifyIDs.push(trackId)
-
-        //     const trackInfo = await getTrackInfo(trackId, accessToken);
-        //     console.log("Track Info:", trackInfo);
         } catch (error) {
             console.error("Error getting track info:", error);
         }
@@ -30,22 +25,49 @@ async function getSpotifyTrackIDs() {
     return spotifyIDs
 }
 
-getSpotifyTrackIDs();
 
-//change ID key back into int from string!!
-// async function getSpotifyMetadataObjects() {
-//     const tokenResponse = await getToken();
-//     const accessToken = tokenResponse.access_token;
-//     const spotifyIds = getSpotifyTrackIDs();
-//     for (const id of spotifyIds) {
+// change ID key back into int from string!!
+async function getSpotifyMetadataObjects() {
+    const allMetaObjects = [];
+    const tokenResponse = await getToken();
+    const accessToken = tokenResponse.access_token;
+    const spotifyIds = await getSpotifyTrackIDs();
+    console.log("testing output of dictionary:", spotifyIds)
+    for (const key in spotifyIds) {
+        if (spotifyIds.hasOwnProperty(key)) {
+            const value = spotifyIds[key];
 
-//     }
-//     const trackInfo = await getTrackInfo(trackId, accessToken);
-//        console.log("Track Info:", trackInfo);
+            const trackInfo = await getTrackInfo(value , accessToken);
+            console.log("Track Info:", trackInfo);
 
+            const trackMeta = await getTrackMetadata(value , accessToken);
+            console.log("Track Info:", trackMeta);
+            let customObject = {
+                wikiID: parseInt(key), 
+                spotifyID: value,
+                songName: trackMeta.name, 
+                artistName: trackMeta.artists[0].name, 
+                year: parseInt(trackMeta.album.release_date.slice(0, 4)), 
+                genre: trackMeta.genres[0], 
+                acousticness: trackInfo.acousticness,
+                danceability: trackInfo.danceability,
+                duration_ms: trackInfo.duration_ms,
+                energy: trackInfo.energy,
+                instrumentalness: trackInfo.instrumentalness,
+                time_signature: trackInfo.time_signature,
+                key: trackInfo.key,
+                liveness: trackInfo.liveness,
+                loudness: trackInfo.loudness,
+                tempo: trackInfo.tempo,
+                valence: trackInfo.valence
+            };
+            allMetaObjects.push(customObject);
+            console.log("Custom Object:", customObject)
+        }
+    }
+}
 
-
-// }
+getSpotifyMetadataObjects();
 
 let customObject = {
     wikiID: 0,
@@ -55,10 +77,19 @@ let customObject = {
     year: 0,
     genre: "",
     acousticness: 0,
-
+    danceability: 0,
+    duration_ms: 0,
+    energy: 0,
+    instrumentalness: 0,
+    time_signature: 0,
+    key: 0,
+    liveness: 0,
+    loudness: 0,
+    tempo: 0,
+    valence: 0
 }
 
-// export {getSpotifyMetadataObjects}
+export {getSpotifyMetadataObjects}
 
 // getToken().then(response => {
 //     getTrackInfo("11dFghVXANMlKmJXsNCbNl", response.access_token).then(profile => {
